e6d0c86963d9f9174447591b994f6e6f5bcb52d0
Sheersho Pramanik <sheprama@WKMZT7AE0F99.global.publicisgroupe.net>
2023-02-27 00:07:15 +0530

    added header component and css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
-import './App.css';
-import Product from './Components/Products';
-
+import './index';
+import { Fragment } from 'react';
+import Header  from './components/Layout/Header';
 
 function App() {
   return (
-
-    <div className="App">
-      <h1>My Demo Shop</h1>
-      <Product title="Product 1" price="10" description="First product" />
-      <Product title="Product 2" price="20" description="Second product" />
-    </div>
-
+    <Fragment>
+        <Header />
+    </Fragment>
   )
 }
 
