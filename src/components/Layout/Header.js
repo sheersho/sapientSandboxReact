9c3a79df26ceb15cbe3425f175f24002c30bc34e
Sheersho Pramanik <sheprama@WKMZT7AE0F99.global.publicisgroupe.net>
2023-02-27 08:08:07 +0530

    added header and cart icon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,18 @@
 import { Fragment } from "react";
-import mealsImage from '../../assets/meals.jpg'
-import classes from './header.module.css'
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
       </header>
-      <img className={classes['main-image']} src={mealsImage}/> 
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="meals-banner" />
+      </div>
     </Fragment>
   );
 };
