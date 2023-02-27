6725a690de6fe6c9db92c10b96fe1fed695e6ddd
Sheersho Pramanik <sheprama@WKMZT7AE0F99.global.publicisgroupe.net>
2023-02-27 08:45:58 +0530

    added meals

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './index';
 import { Fragment } from 'react';
 import Header  from './components/Layout/Header';
+import Meals from './components/Meals/Meals'
 
 function App() {
   return (
     <Fragment>
         <Header />
+        <main>
+          <Meals/>
+        </main>
     </Fragment>
   )
 }
