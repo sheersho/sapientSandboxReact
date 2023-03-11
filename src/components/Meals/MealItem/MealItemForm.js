64950148a15eecedabc54a60d51ade81a4e48af4
Sheersho Pramanik <sheprama@WKMZT7AE0F99.global.publicisgroupe.net>
2023-03-11 17:36:15 +0530

    new cart modal

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -5,11 +5,12 @@ const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
             <Input label="Amount" input={{
-                id: 'amount',
+                id: 'amount_' + props.id,
                 type: 'number',
                 min: '1',
                 max: '5',
-                step : '1'
+                step : '1',
+                defaultValue: '1'
             }} />
             <button>+ Add</button>
         </form>
