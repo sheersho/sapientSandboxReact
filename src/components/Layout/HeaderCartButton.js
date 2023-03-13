bd4eb2b2043852c2d3959190e6f50fa574974918
Sheersho Pramanik <sheprama@WKMZT7AE0F99.global.publicisgroupe.net>
2023-03-13 08:17:46 +0530

    new cart modal

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,12 +1,24 @@
-import CartIcon from '../Cart/CartIcon'
-import classes from './HeaderCartButton.module.css'
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context"
+import { useContext } from "react";
 
-const HeaderCartButton = props => {
-    return <button className={classes.button}>
-        <span className={classes.icon}><CartIcon /></span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+const HeaderCartButton = (props) => {
+
+  const cartCtx = useContext(CartContext)
+  const numberOfCartItems = cartCtx.items.reduce((curNumber,item) => {
+    return curNumber + item.amount  
+  },0)
+
+  return (
+    <button className={classes.button} onClick={props.onShowCart1}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-}
+  );
+};
 
 export default HeaderCartButton;
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,14 @@
 import classes from './Input.module.css';
+import React from 'react';
 
-const Input = (props) => {
-    return <div className = {classes.input}>
+const Input = React.forwardRef((props, ref) => {
+    return (
+        <div className = {classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input id={props.input.id} {...props.input}/>
+        <input ref={ref} id={props.input.id} {...props.input}/>
 
     </div>
-
-}
+   );
+});
 
 export default Input;
